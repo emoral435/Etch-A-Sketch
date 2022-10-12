d887586737bccfefe2d8a679e5893a0682f3fc47
Eduardo Morales <emoral435@gmail.com>
2022-10-12 16:53:41 -0500

    Finished overall look, and added JS to slider

---
+++ src/app.js
@@ -0,0 +1,8 @@
+let gridOutput = document.querySelector("#rangeOutput");
+let slider = document.querySelector("#slider")
+
+gridOutput.innerHTML = `${slider.value} x ${slider.value}`
+
+slider.oninput = function() {
+    gridOutput.innerHTML = `${this.value} x ${this.value}`
+}
