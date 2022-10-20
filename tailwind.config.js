452de35366c1f24b596d3b884f307a486c6c4f6e
Eduardo Morales <emoral435@gmail.com>
2022-10-19 22:05:10 -0500

    Trial for mobile version

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    screens: {
+      'xs': '480px',
+
+      'sm': '640px',
+
+      'md': '768px',
+
+      'lg': '1024px',
+
+      'xl': '1280px',
+
+      '2xl': '1536px',
+
+    },
     extend: {
       rotate: {
         '360': '360deg',
-      }
+      },
     },
   },
   plugins: [],
